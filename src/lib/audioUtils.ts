3f06b8766f1b102c5813b55e1bf5c960ae550d71
ceuk <ceuk.dev@gmail.com>
2021-03-02 00:48:51 +0000

    imperative version of audioUtils is more readable

--- src/lib/audioUtils.ts
+++ src/lib/audioUtils.ts
@@ -1,56 +1,37 @@
-import { adjust, divide, sum, ifElse, propEq, prop, map, append, prepend, __, reduce, addIndex, construct, multiply } from 'ramda'
-import { createPipe, pipe } from 'remeda'
+const inputSampleRate = 44100
 
-const reduceIdx = addIndex(reduce)
-
-// createDataView :: buffer -> int -> int -> buffer -> DataView
-const createDataView = createPipe(
-  (buf: Buffer) => buf.length,
-  multiply(2),
-  construct(ArrayBuffer),
-  construct(DataView)
-)
-
-// pcmEncode :: buffer -> buffer
-export function pcmEncode (input: Buffer) {
-  const view = createDataView(input)
-  // Using recursion would be cleaner but much more computationally
-  // expensive until TCO is properly implemented in ECMAScript
-  // eslint-disable-next-line no-loops/no-loops, immutable/no-let
-  for (let i = 0, offset = 0; i < input.length; i++, offset += 2) {
-    const s = Math.max(-1, Math.min(1, input[i]))
+export function pcmEncode (input: Float32Array) {
+  var offset = 0
+  var buffer = new ArrayBuffer(input.length * 2)
+  var view = new DataView(buffer)
+  for (var i = 0; i < input.length; i++, offset += 2) {
+    var s = Math.max(-1, Math.min(1, input[i]))
     view.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7FFF, true)
   }
-  return view.buffer
+  return buffer
 }
 
-interface DownsampleBufferArgs { buffer: Uint8Array, outputSampleRate: number }
-export const downsampleBuffer = ({ buffer, outputSampleRate = 16000 }: DownsampleBufferArgs) => {
-  const ratio = 44100 / outputSampleRate
-  return ifElse(
-    // if
-    propEq('outputSampleRate', 44100),
-    // then
-    prop('buffer'),
-    // else
-    createPipe(
-      prop('buffer'),
-      reduceIdx((acc: any, val, i) =>
-        pipe(
-          ratio,
-          divide(i + 0.1),
-          Math.floor,
-          (number: number) => Array.isArray(acc[number])
-            ? adjust(
-                number,
-                prepend(val),
-                acc
-              )
-            : () => append([val], acc)
-        ),
-      []
-      ),
-      map((values: number[]) => sum(values) / values.length)
-    )
-  )({ buffer, outputSampleRate })
+export function downsampleBuffer ({ buffer, outputSampleRate = 16000 }: { buffer: Float32Array, outputSampleRate: number }) {
+  if (outputSampleRate === inputSampleRate) {
+    return buffer
+  }
+
+  var sampleRateRatio = inputSampleRate / outputSampleRate
+  var newLength = Math.round(buffer.length / sampleRateRatio)
+  var result = new Float32Array(newLength)
+  var offsetResult = 0
+  var offsetBuffer = 0
+  while (offsetResult < result.length) {
+    var nextOffsetBuffer = Math.round((offsetResult + 1) * sampleRateRatio)
+    var accum = 0
+    var count = 0
+    for (var i = offsetBuffer; i < nextOffsetBuffer && i < buffer.length; i++) {
+      accum += buffer[i]
+      count++
+    }
+    result[offsetResult] = accum / count
+    offsetResult++
+    offsetBuffer = nextOffsetBuffer
+  }
+  return result
 }
